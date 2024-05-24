26092032ebf849fdbcde27f4150decb4176bfd2a
deividsonsabino <deividsonsabino@outlook.com.br>
2024-05-24 19:25:59 -0300

    :sparkle: feat: Data stuff and set player to collect objects

--- Assets/Scripts/Character/CharacterInteract.cs
+++ Assets/Scripts/Character/CharacterInteract.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class CharacterInteract : MonoBehaviour
 {
+    PlayerCharacter playerCharacter;
     [SerializeField] Transform pivot;
     [SerializeField] Vector3 interactAreaSize = Vector3.one;
-public void Interact()
+
+    private void Awake()
+    {
+        playerCharacter = GetComponent<PlayerCharacter>();
+    }
+
+    public void Interact()
     {
         Collider[] colliders = Physics.OverlapBox(pivot.position, interactAreaSize);
 
@@ -15,7 +22,7 @@ public void Interact()
             Interactable interactable = colider.GetComponent<Interactable>();
             if (interactable != null)
             {
-                interactable.Interact();
+                interactable.Interact(playerCharacter);
                 break;
             }
         }
---
+++ Assets/Scripts/MoneyPickUp.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyPickUp : MonoBehaviour
+{
+    [SerializeField] int money;
+    public void PickUp(PlayerCharacter playerCharacter)
+    {
+        playerCharacter.AddMoney(money);
+    }
+  
+}
